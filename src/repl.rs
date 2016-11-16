b562d06e3a78f1a2c808022358486cd17f66c95f
Michael Sheely <msheely@hmc.edu>
2016-11-16 14:19:31 -0800

    Fixed parse error in the case of left over input

--- src/repl.rs
+++ src/repl.rs
@@ -16,9 +16,10 @@ fn main() {
         match ir::parse(line.as_str().trim().as_bytes()) {
             IResult::Done(rest, ref res) if rest.len() == 0 =>
                 println!("Parsed: {:#?}", res),
+            IResult::Done(_, ref res) =>
+                println!("Remaining inputp after parsing {:#?}", res),
             IResult::Error(x) => println!("Error: {:?}", x),
             IResult::Incomplete(x) => println!("Incomplete: {:?}", x),
-            _ => println!("Not error or incomplete, but unsuccessful parse."),
         }
         line.clear();
     }
