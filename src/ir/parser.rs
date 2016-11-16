f4e4691eb72f5ca9a7e3932255c0f1e2472e92df
Michael Sheely <msheely@hmc.edu>
2016-11-16 14:01:39 -0800

    Allow for multiple days and free times, and whitespace insensitivity.

--- src/ir/parser.rs
+++ src/ir/parser.rs
@@ -15,10 +15,11 @@ named!(tues <Day>,
        chain!(alt!(tag!("Tuesday") | tag!("tuesday")), || { Day::Tuesday }));
 
 named!(pub day <Day>, alt!(
-       chain!(mon, || { Day::Monday }) |
-       chain!(tues, || { Day::Tuesday })
-   )
+       chain!(d : alt!(mon | tues) ~ opt!(char!(',')) ~ whitespace, || { d }))
 );
+
+// Zero or more instances of whitespace
+named!(whitespace<Vec<&[u8]> >, many0!(tag!(" ")));
 
 named!(meridiem <Meridiem>, alt!(
         chain!(char!('a') ~ alt!(tag!(".m.") | tag!("m")), || {Meridiem::am}) |
@@ -30,18 +31,18 @@ named!(hour <u8>,
        chain!(d1 : digit, || { String::from_utf8_lossy(d1).parse().unwrap() })
 );
 
-named!(pub time <Time>, chain!(h : hour ~ m : meridiem, 
+named!(pub time <Time>, chain!(h : hour ~ m : meridiem,
        || Time{hour: h, meridiem: m})
 );
 
 named!(displacement <Displacement>, chain!(
         tag!("free(") ~
-        t0 : time ~ char!('-') ~ 
+        t0 : time ~ char!('-') ~
         t1 : time ~ char!(')'), || Displacement{start: t0, end: t1, badness: 0})
 );
 
-named!(pub entry <Entry>, 
-        chain!(days : many1!(day) ~ tag!(":") ~
+named!(pub entry <Entry>,
+        chain!(days : many1!(day) ~ tag!(":") ~ whitespace ~
                displacements : many1!(displacement),
                || Entry{days: days, displacements: displacements} )
 );
