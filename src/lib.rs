10975aff4a24b5c64a14f5303aa0f91cdac5b029
Michael Sheely <msheely@hmc.edu>
2016-12-04 23:40:52 -0800

    Added tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
+
 #[cfg(test)]
 mod tests {
+    //use super::*;
     #[test]
-    fn it_works() {
+    fn time_conversions() {
+        //let t = Time{hour: 3, meridiem: pm}.to24hr();
+        /*
+        println!("{}", ir::Time{hour: 1, meridiem: Meridiem::pm}.to24hr());
+        println!("{}", ir::Time{hour: 1, meridiem: Meridiem::am}.to24hr());
+        println!("{}", ir::Time{hour: 12, meridiem: Meridiem::am}.to24hr());
+        println!("{}", ir::Time{hour: 12, meridiem: Meridiem::pm}.to24hr());
+        println!("{}", ir::Time{hour: 9, meridiem: Meridiem::pm}.to24hr());
+        */
         assert_eq!(1, 1);
     }
 }
