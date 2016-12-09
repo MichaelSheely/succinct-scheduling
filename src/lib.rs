6a88f944b45f82dc802c58f4b94620e5d3ad62e4
Michael Sheely <msheely@hmc.edu>
2016-12-08 20:00:29 -0800

    Fixed tests to actually test Time conversion

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,15 @@ mod ir;
 
 #[cfg(test)]
 mod tests {
-    //use super::*;
     #[test]
     fn time_conversions() {
         use super::ir;
-        let t = ir::Time{hour: 3, meridiem: ir::Meridiem::pm}.to24hr();
-        /*
-        println!("{}", ir::Time{hour: 1, meridiem: Meridiem::pm}.to24hr());
-        println!("{}", ir::Time{hour: 1, meridiem: Meridiem::am}.to24hr());
-        println!("{}", ir::Time{hour: 12, meridiem: Meridiem::am}.to24hr());
-        println!("{}", ir::Time{hour: 12, meridiem: Meridiem::pm}.to24hr());
-        println!("{}", ir::Time{hour: 9, meridiem: Meridiem::pm}.to24hr());
-        */
-        assert_eq!(1, 1);
+        use super::ir::Meridiem;
+
+        assert_eq!(1,  ir::Time{hour:  1, meridiem: Meridiem::am}.to24hr());
+        assert_eq!(15, ir::Time{hour:  3, meridiem: Meridiem::pm}.to24hr());
+        assert_eq!(13, ir::Time{hour:  1, meridiem: Meridiem::pm}.to24hr());
+        assert_eq!(21, ir::Time{hour:  9, meridiem: Meridiem::pm}.to24hr());
+        assert_eq!(12, ir::Time{hour: 12, meridiem: Meridiem::pm}.to24hr());
     }
 }
