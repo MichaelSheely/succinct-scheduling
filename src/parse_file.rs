5e092c9e6e6c00ed8b20e214de3086989c87f327
Michael Sheely <msheely@hmc.edu>
2016-12-09 22:14:25 -0800

    Code cleanup and removed hardcoded filename allowing user to input the filename through argv or stdin

--- src/parse_file.rs
+++ src/parse_file.rs
@@ -7,33 +7,20 @@ extern crate nom;
 mod ir;
 
 use nom::IResult;
-use ir::Entry;
-use ir::Day;
-use ir::Time; // delete
-use ir::Meridiem; // delete
-use std::io::Read;
+use ir::{Entry, Day, Time};
+use ir::{day_to_int, day_from_int};
+use ir::{NUM_DAYS, NUM_HOURS, NUM_MINUTES};
 use std::fs::File;
+use std::io;
+use std::io::Read;
+use std::io::stdin;
+use std::env;
 
 // TODO make into an object with this as new() method
 fn empty_schedule() -> Vec<bool> {
-    let num_slots = 7 * 24; //* 60; // 7 days with 24 hours with 60 mins
+    let num_slots = (NUM_DAYS * NUM_HOURS * NUM_MINUTES) as usize;
     let schedule = vec![false; num_slots];
-    /*
-    //let days = vec![Day::Monday, Day::Tuesday, Day::Wednesday];
-    let days = vec![0, 1, 2];
-    for day in days {
-        schedule.insert(day, Vec::new());
-    }
-    */
     schedule
-}
-
-fn day_int(d: &Day) -> u8 {
-    match *d {
-        Day::Monday    => 0,
-        Day::Tuesday   => 1,
-        Day::Wednesday => 2
-    }
 }
 
 fn enter_times(schedule: &mut Vec<bool>, entries: &Vec<Entry>) {
@@ -42,43 +29,49 @@ fn enter_times(schedule: &mut Vec<bool>, entries: &Vec<Entry>) {
         let displacements = &entry.displacements;
         for day in days {
             for disp in displacements {
-                println!("Adding {} to {:?}.", disp, day);
-                let day_int = day_int(day) as usize;
+                // TODO: Support minutes as well
+                println!("Free from {} to {:?}.", disp, day);
+                let day_int = day_to_int(day) as usize;
                 let mut start_int = day_int * 24 + disp.start.to24hr() as usize;
                 let end_int = day_int * 24 + disp.end.to24hr() as usize;
-                println!("From {} to {}.", start_int, end_int);
-                //TODO assert start_int < end_int
                 while start_int < end_int {
                     schedule[start_int] = true;
                     start_int += 1;
                 }
-                // TODO acutally add this
-                // displacement to appropriate vector
             }
         }
     }
-    for (i, freedom) in schedule.iter().enumerate() {
-        //println!("Slot {} is {}", i, freedom);
-    }
+}
+
+fn get_filename() -> String {
+    println!("Enter the name of the file that contains your schedule.");
+    let mut line = String::new();
+    io::stdin().read_line(&mut line).map(|l| l > 0).unwrap_or(false);
+    String::from(line.as_str().trim())  // Remove newline
 }
 
 fn main() {
     let mut schedule = empty_schedule();
     let mut line = String::new();
-    let mut f = File::open("schedule2.txt").unwrap();
+    let mut fname : String;
+    match env::args().nth(1) {
+        Some(name) => fname = name,
+        _ => fname = get_filename(),
+    }
+    println!("Got filename <{}>", fname);
+
+    let mut f = File::open(&fname).unwrap();
     f.read_to_string(&mut line).unwrap();
-    println!("Read data from provided file.");
-    {
-        match ir::parse(line.as_str().trim().as_bytes()) {
-            IResult::Done(rest, ref res) if rest.len() == 0 => {
-                println!("Parsed Schedule.");
-                enter_times(&mut schedule, res);
-            },
-            IResult::Done(_, ref res) =>
-                println!("Remaining input after parsing\n{:#?}", res),
-            IResult::Error(x) => println!("Error: {:?}", x),
-            IResult::Incomplete(x) => println!("Incomplete: {:?}", x),
-        }
+    println!("Read data from {}.", fname);
+    match ir::parse_txt(line.as_str().trim().as_bytes()) {
+        IResult::Done(rest, ref res) if rest.len() == 0 => {
+            println!("Parsed Schedule.");
+            enter_times(&mut schedule, res);
+        },
+        IResult::Done(_, ref res) =>
+            println!("Remaining input after parsing\n{:#?}", res),
+        IResult::Error(x) => println!("Error: {:?}", x),
+        IResult::Incomplete(x) => println!("Incomplete: {:?}", x),
     }
 }
 
