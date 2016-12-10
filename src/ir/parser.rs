7b868b5beff03e855546abffb522d04836fc8470
Michael Sheely <msheely@hmc.edu>
2016-12-09 22:13:58 -0800

    Added parsing of isc files (google calendar)

--- src/ir/parser.rs
+++ src/ir/parser.rs
@@ -1,9 +1,10 @@
+//#[macro_use] extern crate lazy_static;
+extern crate regex;
+use self::regex::Regex;
 
-use ir::Day;
-use ir::Displacement;
-use ir::Entry;
-use ir::Meridiem;
-use ir::Time;
+use ir::day_to_int;
+use ir::{Day, Displacement, Entry, Meridiem, Time};
+use ir::NUM_DAYS;
 
 use nom::digit;
 
@@ -74,4 +75,60 @@ named!(pub entry <Entry>,
 named!(pub entries <Vec<Entry> >,
        many1!(entry)
 );
+
+// Below this point we create parsers for calendar documents
+
+fn adjust_timezone(h: u8) -> u8 {
+    if h > 7 {
+        h - 8
+    } else {
+        h + 16
+    }
+}
+
+fn date_to_day(month: u8, date: u8) -> Day {
+    if month != 12 {
+        unimplemented!();
+    }
+    match date % 7 {
+        0 => Day::Wednesday,
+        //1 => Day::Thursday,
+        //2 => Day::Friday,
+        //3 => Day::Saturday,
+        //4 => Day::Sunday,
+        5 => Day::Monday,
+        6 => Day::Tuesday,
+        _ => panic!(),
+    }
+}
+
+fn empty_displacements() -> Vec<Vec<Displacement>> {
+    vec![Vec::<Displacement>::new(); NUM_DAYS as usize]
+}
+
+pub fn parse_ics(contents: &str) -> Vec<Vec<Displacement>> {
+    let mut displacements = empty_displacements();
+    let re = Regex::new(r"(?x)(\d{4})(\d{2})(\d{2})T(\d{2}).*\r\n
+                        DTEND:(\d{4})(\d{2})(\d{2})T(\d{2}).*\r\n").unwrap();
+    let mut iter = contents.split("DTSTART:");
+    iter.next();  // skip first chunk, doesn't start with times
+    for s in iter {
+        for cap in re.captures_iter(s) {
+            // For some reason, time zone is 8 hours ahead???
+            let mut start_hour = cap.at(4).unwrap().parse::<u8>().unwrap();
+            start_hour = adjust_timezone(start_hour);
+            let mut end_hour = cap.at(8).unwrap().parse::<u8>().unwrap();
+            end_hour = adjust_timezone(end_hour);
+            //println!("{}/{}: {} - {}",
+            //cap.at(2).unwrap_or(""), cap.at(3).unwrap_or(""),
+            //start_hour, end_hour);
+            let date = cap.at(3).unwrap().parse::<u8>().unwrap();
+            displacements[day_to_int(&date_to_day(12, date)) as usize].push(
+              Displacement{start: Time::from24hr(start_hour),
+                           end: Time::from24hr(end_hour),
+                           badness : 0} );
+        }
+    }
+    displacements
+}
 
