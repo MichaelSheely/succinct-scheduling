90d5ee8eb8908fc2184cc3714271bc7b5d4ea2ea
Michael Sheely <msheely@hmc.edu>
2016-12-09 22:13:21 -0800

    Added exports

--- src/ir/mod.rs
+++ src/ir/mod.rs
@@ -5,5 +5,7 @@ mod parser;
 
 pub use self::ir::{Day, Time, Meridiem, Displacement, Entry};
 pub use self::ir::{day_to_int, day_from_int};
-pub use self::parser::entries as parse;
+pub use self::ir::{NUM_DAYS, NUM_HOURS, NUM_MINUTES};
+pub use self::parser::entries as parse_txt;
+pub use self::parser::parse_ics;
 pub use self::parser::range as parse_time_range;
